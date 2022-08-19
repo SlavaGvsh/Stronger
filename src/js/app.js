05af93cee91cde8fb160587e0062119cf9fb6b10
SlavaGvsh <slava.gvsh@gmail.com>
2022-08-19 10:24:28 +0300

    add icon

--- src/js/app.js
+++ src/js/app.js
@@ -122,71 +122,14 @@ const swiperStories = new Swiper(".stories .swiper", {
 
 });
 
-if (document.documentElement.clientWidth > 767) {
-   let items_pricing = document.querySelectorAll('.pricing .carousel .carousel-item')
+// include('../../../node_modules/@popperjs/core/dist/umd/popper.js')
+// include('../../node_modules/bootstrap/dist/js')
 
-   items_pricing.forEach((el) => {
-      const minPerSlide = 3
-      let next = el.nextElementSibling
-      for (var i = 1; i < minPerSlide; i++) {
-         if (!next) {
-            // wrap carousel by using first child
-            next = items_pricing[0]
-         }
-         let cloneChild = next.cloneNode(true)
-         el.appendChild(cloneChild.children[0])
-         next = next.nextElementSibling
-      }
-   })
+// Импортируем Popper
+//=include('../../../node_modules/@popperjs/core/dist/umd/popper.js')
+// include('../../node_modules/@popperjs/core/dist/umd/popper.js')
 
-   // let items_classes = document.querySelectorAll('.classes .carousel .carousel-item')
-
-   // items_classes.forEach((el) => {
-   //    const minPerSlide = 3
-   //    let next = el.nextElementSibling
-   //    for (var i = 1; i < minPerSlide; i++) {
-   //       if (!next) {
-   //          // wrap carousel by using first child
-   //          next = items_classes[0]
-   //       }
-   //       let cloneChild = next.cloneNode(true)
-   //       el.appendChild(cloneChild.children[0])
-   //       next = next.nextElementSibling
-   //    }
-   // })
-
-   let items_trainers = document.querySelectorAll('.trainers .carousel .carousel-item')
-
-   items_trainers.forEach((el) => {
-      const minPerSlide = 2
-      let next = el.nextElementSibling
-      for (var i = 1; i < minPerSlide; i++) {
-         if (!next) {
-            // wrap carousel by using first child
-            next = items_trainers[0]
-         }
-         let cloneChild = next.cloneNode(true)
-         el.appendChild(cloneChild.children[0])
-         next = next.nextElementSibling
-      }
-   })
-
-   let items_stories = document.querySelectorAll('.stories .carousel .carousel-item')
-
-   items_stories.forEach((el) => {
-      const minPerSlide = 2
-      let next = el.nextElementSibling
-      for (var i = 1; i < minPerSlide; i++) {
-         if (!next) {
-            // wrap carousel by using first child
-            next = items_stories[0]
-         }
-         let cloneChild = next.cloneNode(true)
-         el.appendChild(cloneChild.children[0])
-         next = next.nextElementSibling
-      }
-   })
-}
-
-
-
+// Импортируем Bootstrap 5
+// =include('../../../node_modules/bootstrap/dist/js/bootstrap.js')
+// include('../../node_modules/bootstrap/dist/js/bootstrap.js')
+// include('../../node_modules/swiper/swiper-bundle.min.js')
